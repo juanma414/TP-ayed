dd0614bea6b7c32f9a1da8ec03e44f7866105bd7
juan ltv <juanmanueltarrago@gmail.com>
2021-04-27 16:33:35 -0300

    correccion de errores

--- tp1.py
+++ tp1.py
@@ -1,56 +1,56 @@
-"""const = 123
+const = 123
 contContra = 0
 contR = 0
 contC = 0
 contBA = 0
 max = 0
 x = int(input("1. Empresas 2. Clientes 0. Salir"))
-if x == 1:"""
-    print("ingrese clave")
-	while contContra < 3:
+if x == 1:
+print("ingrese clave")
+while contContra < 3:
 	 	 
 	   
-	   y = int(input())
-	   if y==const:
-		  """print("Oprima 1 para alta de empresa 0. para ir al menú principal")
-		  c = int(input())
-		  if c == 1:
-			 while codEmp != 0:
-			 print("ingrese nombre")
-			 nombre = str(input())
-			 print("ingrese Dirección")
-			 direccion = str(input())
-			 print("ingrese Mail")
-			 mail = str(input())
-			 print("ingrese COD/CIUDAD")
-			 codCiudad = str(input())
-			 print("ingrese Nombre Ciudad")
-			 nombreCiudad = str(input())
-			 if codCiudad =="ROS":
+	y = int(input())
+	if y==const:
+	print("Oprima 1 para alta de empresa 0. para ir al menú principal")
+	c = int(input())
+		if c == 1:
+			while codEmp != 0:
+			print("ingrese nombre")
+			nombre = str(input())
+			print("ingrese Dirección")
+			direccion = str(input())
+			print("ingrese Mail")
+			mail = str(input())
+			print("ingrese COD/CIUDAD")
+			codCiudad = str(input())
+			print("ingrese Nombre Ciudad")
+			nombreCiudad = str(input())
+			if codCiudad =="ROS":
 				contR = contR + 1
-			 elif codCiudad == "BA":
+			elif codCiudad == "BA":
 				contBA = contBA +1
-			 else contC = contC + 1
+			else contC = contC + 1
 
-			 if contR > max:
+			if contR > max:
 			 	max = contR
 				nMax = nombreCiudad
-			 elif contC > max:
+			elif contC > max:
 				max = contC
 				nMax = nombreCiudad
 
-			 elif contBA > max:
+			elif contBA > max:
 				max = contBA
 				nMax = nombreCiudad
-			 print("ingrese COD/EMP o 0 para volver al menú principal")
+			print("ingrese COD/EMP o 0 para volver al menú principal")
 			 codEmp = int(input())
-		  print("la ciudad que tiene mas empresas desarrolladoras es:",nMax,"y la cantidad es",max)
-		  print("la ciudad de Rosario tiene ",contR,",la ciudad de Córdoba tiene ",contC,"y la ciudad de Buenos Aires tiene ",contBA)
-	"""
-	   else:
-		  print("contraseña incorrecta intente nuevamente")
-		  contContra == contContra + 1 
-	   if contContra == 3 :
+		print("la ciudad que tiene mas empresas desarrolladoras es:",nMax,"y la cantidad es",max)
+		
+	
+	else:
+		print("contraseña incorrecta intente nuevamente")
+		contContra == contContra + 1 
+	if contContra == 3 :
 
 	   print("ha superado el numero maximo de intentos")
 
--- tp2.py.py
+++ tp2.py.py
@@ -5,7 +5,57 @@ contBA = 0
 contC = 0
 max = 0
 x = int(input("1.Empresas 2.Clientes 0.Salir:"))
-    if x == 1:
-        print("ingrese clave")
-    elif x ==2:
-        print("no")    
+if x==1:
+	print("ingrese clave")
+	for contContra in range(3):
+		y = int(input())
+		if y==const:
+			print("oprima 1 para alta de Empresas 0 para ir al menú principal")
+			c = int(input())
+			if c == 1:
+				while codEmp!=0:
+					codEmp=int(input("ingrese codigo de empresa o 0 para volver al menú principal"))
+					nombre=str(input("ingrese nombre"))
+					direccion=str(input("ingrese dirección"))
+					mail=str(input("ingrese mail"))
+					codCiudad = str(input("ingrese COD/CIUDAD"))
+					nombreCiudad=str(input("ingrese el nombre de la ciudad"))
+					if codCiudad=="ROS":
+						contR==contR+1
+					elif codCiudad=="CBA":
+						contC==contC+1
+					elif codCiudad=="BA":
+						contBA==contBA+1
+
+					if contR>max:
+						max == contR
+						nMax == "Rosario"
+					elif contBA>max:
+						max == contBA
+						nMax == "Buenos Aires"
+					elif contC>max:
+						max == contC
+						nMax == "Córdoba"
+							
+
+						
+
+					
+				print("la ciudad que tiene más empresas desarrolladoras es",nMax,"y tiene",max)
+				print("la ciudad de Rosario tiene ",contR,",la ciudad de Córdoba tiene ",contC,"y la ciudad de Buenos Aires tiene ",contBA)
+		else:
+			print("contraseña incorrecta intente nuevamente")
+			contContra==contContra+1
+			if contContra==2:
+				print("ha superado el maximo de intentos posibles")
+
+elif x == 2:
+	print("programa en proceso")
+
+				
+
+			
+
+
+
+   
